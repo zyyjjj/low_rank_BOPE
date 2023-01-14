270bb9d4a72aef6d5a535f8c81a71780ace3d678
zyyjjj <yz685@cornell.edu>
2023-01-14 14:03:26 -0500

    develop test case for portflio problem

--- low_rank_BOPE/test_problems/portfolio_opt_surrogate/portfolio_surrogate_test.py
+++ low_rank_BOPE/test_problems/portfolio_opt_surrogate/portfolio_surrogate_test.py
@@ -1,8 +1,12 @@
 import torch
-from portfolio_surrogate import DistributionalPortfolioSurrogate
+from botorch.utils.sampling import draw_sobol_samples
+from portfolio_surrogate import DistributionalPortfolioSurrogate, utility_risk_measures
+
 
 simulator = DistributionalPortfolioSurrogate()
 torch.manual_seed(0)
+
+# test on single sample
 X = torch.zeros(torch.Size([3, 1, 3]))
 X[..., 0] += 0.0264
 X[..., 1] += 0.9377
@@ -11,11 +15,23 @@ X[..., 2] += 0.1512
 print(X)
 print(simulator.evaluate_true(X))
 
-# torch.manual_seed(0)
-# X = torch.zeros(torch.Size([3, 1, 5]))
-# X[..., 0] += 0.2694
-# X[..., 1] += 0.2327
-# X[..., 2] += 0.2552
-# X[..., 3:5] = torch.rand(torch.Size([3, 1, 2]))
-# print(X)
-# print(simulator.evaluate_true(X))
+
+# test on batch samples, test utility function
+X = draw_sobol_samples(
+    bounds = torch.Tensor([[0,0,0], [1,1,1]]),
+    n=1,
+    q=30
+).squeeze(0)
+print('X shape', X.shape)
+Y = simulator.evaluate_true(X)
+print('Y shape', Y.shape)
+
+U, S, V = torch.svd(Y)
+S_squared = torch.square(S)
+explained_variance = S_squared / S_squared.sum()
+
+print('explained variance', explained_variance)
+
+util_vals = utility_risk_measures(Y, util_func_key='mean_plus_sd')
+print('Y', Y)
+print('util_vals', util_vals)
