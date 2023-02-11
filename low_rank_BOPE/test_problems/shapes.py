804ec2dfeb8c5387172d5e77f5295d30a5c24d19
zyyjjj <yz685@cornell.edu>
2023-02-11 16:11:58 -0500

    implement and test funcs for generating imgs and areas

--- low_rank_BOPE/test_problems/shapes.py
+++ low_rank_BOPE/test_problems/shapes.py
@@ -1,17 +1,31 @@
+from typing import Optional
+
 import torch
+from botorch.test_functions.synthetic import SyntheticTestFunction
 from torch import Tensor
 
 # outcome function
 
-class Image():
+def swap(a,b):
+    tmp = a
+    a=b
+    b=tmp
+
+    return a, b
+
+class Image(SyntheticTestFunction):
     r"""
     Class for generating rectangle images
     """
+    dim = 4
+    _bounds = torch.tensor([[0, 1], [0, 1], [0, 1], [0,1]])
+
     def __init__(self, num_pixels: int = 16):
+        super().__init__()
         self.num_pixels = num_pixels
         self.pixel_size = 1 / self.num_pixels
     
-    def forward(self, X):
+    def evaluate_true(self, X):
         r"""
         Args:
             X: 4-dimensional input
@@ -20,33 +34,77 @@ class Image():
         """
 
         # map real values in X to integer indices of pixels
-        pixel_idcs = torch.div(X, 1/self.pixel_size, rounding_mode="floor")
+        pixel_idcs = torch.div(X, self.pixel_size, rounding_mode="floor")
 
-        # pixel_idx[..., 0:2] specify the upper left corner
-        # pixel_idx[..., 2:4] specify the lower right corner
-        # resulting image 
+        Y = torch.zeros((*X.shape[:-1], self.num_pixels**2))
 
-        # TODO: there must be a better way to handle this in batch
+        for sample_idx in range(X.shape[-2]): # TODO: there could be shape erros
 
-        Y = torch.zeros((X.shape[:-1], self.num_pixels**2))
+            row_start, col_start, row_end, col_end = pixel_idcs[sample_idx].numpy().astype(int)
 
-        for sample_idx in range(X.shape[0]): # TODO: could be shape erros
-
-            row_start, col_start, row_end, col_end = pixel_idcs[sample_idx]
-
+            # swap if needed
+            if row_start > row_end:
+                row_start, row_end = swap(row_start, row_end)
+            if col_start > col_end:
+                col_start, col_end = swap(col_start, col_end)
+            
             paint_it_black = [self.num_pixels * r + c \
-                    for r in range(row_start, row_end+1) \
-                    for c in range(col_start, col_end+1)]
+                    for r in range(min(row_start, self.num_pixels-1), min(row_end+1, self.num_pixels)) \
+                    for c in range(min(col_start, self.num_pixels-1), min(col_end+1, self.num_pixels))]
             
             Y[sample_idx, paint_it_black] = torch.ones(1, len(paint_it_black))
 
         return Y
 
 
-# utility function -- TODO: make into class
+# utility function
 
-def area(image):
-    pass
+class AreaUtil(torch.nn.Module):
+    def __init__(self, weights: Optional[Tensor] = None):
+        r"""
+        Args:
+            weights: `1 x outcome_dim` tensor 
+        """
+        super().__init__()
+        self.weights = weights
+    
+    def forward(self, Y: Tensor):
+        area = torch.sum(Y, dim = 1)
+        if self.weights is not None:
+            area = area * self.weights
+        return area
 
-def weighted_area(image, weights):
-    pass
+
+
+
+if __name__ == "__main__":
+
+    NUM_PIXELS = 16
+
+    image_problem = Image(num_pixels = NUM_PIXELS)
+    test_X = torch.tensor(
+        [
+            # edge cases: largest area possible (all pixels black)
+            [0,1,1,0],
+            [1,0,0,1],
+            [1,1,0,0],
+            [0,0,1,1],
+            # edge cases: smallest area possible (pixel on a corner)
+            [0,0,0,0],
+            [1,0,1,0],
+            [0,1,0,1],
+            [1,1,1,1],
+            # generic cases
+            [0.1, 0.1, 0.6, 0.6],
+            [0.1, 0.8, 0.1, 0.6],
+            [0.6, 0.1, 0.1, 0.8],
+            [0.1, 0.6, 0.8, 0.1]
+        ])
+
+    Y = image_problem(test_X)
+    for y in Y:
+        print(torch.reshape(y, (NUM_PIXELS, NUM_PIXELS)))
+    
+    print("computing areas")
+    area_util = AreaUtil()
+    print(area_util(Y))
