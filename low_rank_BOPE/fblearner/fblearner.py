a62daf6c7bfe016fea2c4344b1d0c76ab0a22060
zyyjjj <yz685@cornell.edu>
2023-05-08 11:13:18 -0400

    correct typo and add more documentation

--- low_rank_BOPE/fblearner/fblearner.py
+++ low_rank_BOPE/fblearner/fblearner.py
@@ -88,49 +88,58 @@ def run_one_trial(
     # process data
 
     # key: "method__pestrategy", value: List[{"some metric": float or List[float or List[float]]}]
+    # first cast items in the nested dict from defaultdict to dict
     exp_candidate_results_ = {}
     for k, v in exp.final_candidate_results.items():
         if isinstance(v, defaultdict):
             exp_candidate_results_[k] = dict(v)
+    # then turn nested dict[m][p] to unnested dict[m__p]
     exp_candidate_results__ = {}
-    for key, v in exp_candidate_results_.items():
-        for v_key in v.keys():
-            exp_candidate_results__['__'.join([k, v_key])] = exp_candidate_results_[key][v_key]
+    for k_, v_ in exp_candidate_results_.items():
+        for v_key in v_.keys():
+            exp_candidate_results__['__'.join([k_, v_key])] = exp_candidate_results_[k_][v_key]
 
     # key: "method__pestrategy", value: List[{"some metric": float or List[float or List[float]]}]
+    # first cast items in the nested dict from defaultdict to dict
     PE_session_results_ = {}
-    for k, v in exp.final_candidate_results.items():
+    for k, v in exp.PE_session_results.items():
         if isinstance(v, defaultdict):
             PE_session_results_[k] = dict(v)
+    # then turn nested dict[m][p] to unnested dict[m__p]
     PE_session_results__ = {}
-    for key, v in PE_session_results_.items():
-        for v_key in v.keys():
-            PE_session_results__['__'.join([k, v_key])] = PE_session_results_[key][v_key]
+    for k_, v_ in PE_session_results_.items():
+        for v_key in v_.keys():
+            PE_session_results__['__'.join([k_, v_key])] = PE_session_results_[k_][v_key]
 
     # key: "method__pestrategy", value: {"Y": List, "util_vals": List, "comps": List}
+    # turn dict key from tuple (m, p) to str "m__p" 
     pref_data_dict_ = {}
     for key in exp.pref_data_dict.keys():
         pref_data_dict_['__'.join([key[0], key[1]])] = exp.pref_data_dict[key]
+        # convert tensors under "Y", "util_vals", "comps" to list
         for key_, val_ in pref_data_dict_['__'.join([key[0], key[1]])].items():
-            pref_data_dict_['__'.join([key[0], key[1]])][key_] = val_.tolist() # convert tensor to list
+            pref_data_dict_['__'.join([key[0], key[1]])][key_] = val_.tolist()
 
     # key: "method__pestrategy", value: {"diagnostic": List[float]}
+    # turn dict key from tuple (m, p) to str "m__p" 
     subspace_diagnostics_ = {}
     for key in exp.subspace_diagnostics.keys():
         subspace_diagnostics_['__'.join([key[0], key[1]])] = exp.subspace_diagnostics[key]
+    # cast items from defaultdict to dict
     subspace_diagnostics__ = {}
-    for k, v in subspace_diagnostics_.items():
-        if isinstance(v, defaultdict):
-            subspace_diagnostics__[k] = dict(v)
+    for k_, v_ in subspace_diagnostics_.items():
+        if isinstance(v_, defaultdict):
+            subspace_diagnostics__[k_] = dict(v_)
 
     # key: "method__pestrategy", value: {"X": List, "Y": List}
+    # turn dict key from tuple (m, p) to str "m__p" 
     BO_data_dict_ = {}
     for key in exp.BO_data_dict.keys():
         BO_data_dict_['__'.join([key[0], key[1]])] = exp.BO_data_dict[key]
+        # convert tensors under "X" and "Y" to list
         for key_, val_ in BO_data_dict_['__'.join([key[0], key[1]])].items():
-            BO_data_dict_['__'.join([key[0], key[1]])][key_] = val_.tolist() # convert tensor to list
+            BO_data_dict_['__'.join([key[0], key[1]])][key_] = val_.tolist()
 
-    # these are all defaultdict; if an error occurs, can try casting them to dict 
     return OneRun(
         exp_candidate_results=exp_candidate_results__,
         PE_session_results=PE_session_results__,
